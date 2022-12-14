41b5ed3ec497ac28bca3fecb0ba661d749cbce88
Luka Andrejevic <lukaandrejevic88@gmail.com>
2022-12-14 18:14:58 +0100

    zadatak4

---
+++ src/main/java/d13_12_2022/Zadatak4.java
@@ -0,0 +1,14 @@
+package d13_12_2022;
+
+public class Zadatak4 {
+    public static void main(String[] args) {
+
+        int a = 8, obim = a*3;
+        double p = (a*a * 1.73)/4;
+
+        System.out.println("a:"+a);
+        System.out.println("Povrsina je "+p);
+        System.out.println("Obim je "+obim);
+
+    }
+}
