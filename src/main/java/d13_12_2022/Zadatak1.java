acad62e23d0e9164116f167b505d9335ae66f2c7
Luka Andrejevic <lukaandrejevic88@gmail.com>
2022-12-14 16:48:13 +0100

    prvizadatak

---
+++ src/main/java/d13_12_2022/Zadatak1.java
@@ -0,0 +1,11 @@
+package d13_12_2022;
+
+public class Zadatak1 {
+    public static void main(String[] args) {
+
+        int major = 4, minor = 3, patch = 6;
+
+        System.out.println(major +"." + minor + "." + patch);
+
+    }
+}
