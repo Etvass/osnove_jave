04c9f4cfb64b7c84c5de5de0b5537db1a3ff579e
Luka Andrejevic <lukaandrejevic88@gmail.com>
2022-12-15 07:21:53 +0100

    drugiZadatak

---
+++ src/main/java/d13_12_2022/Zadatak2.java
@@ -0,0 +1,25 @@
+package d13_12_2022;
+
+public class Zadatak2 {
+    public static void main(String[] args) {
+
+        String bkartice = "1234 1232 4321 1212",
+               ime = "Luka",
+               prezime = "Andrejevic";
+
+        int mesec = 10, godina = 25;
+
+        System.out.println("          "+"*******************************************");
+        System.out.println("          "+"*"+"   Credit Card                           "+ "*");
+        System.out.println("          "+"*"+"      ****                               "+ "*");
+        System.out.println("          "+"*"+"      ****                               "+ "*");
+        System.out.println("          "+"*"+"            " + bkartice + "          "+ "*");
+        System.out.println("          "+"*"+"                       valid > " + mesec + "/" + godina + "     "+ "*");
+        System.out.println("          "+"*"+"                                         "+ "*");
+        System.out.println("          "+"*"+"     " + ime + " "+ prezime +"                     "+ "*");
+        System.out.println("          "+"*******************************************");
+
+
+
+    }
+}
