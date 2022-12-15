c77bd369c03eb0ea26e7b145b4d1dc13e3e0c495
Luka Andrejevic <lukaandrejevic88@gmail.com>
2022-12-15 13:03:04 +0100

    Zadatak za vezbanje

---
+++ src/main/java/d14_12_2022/Zadatak6.java
@@ -0,0 +1,63 @@
+package d14_12_2022;
+
+import java.util.Scanner;
+
+public class Zadatak6 {
+    public static void main(String[] args) {
+
+        Scanner mojs = new Scanner(System.in);
+
+        String p = "#";
+
+//------prva_promenljiva----------------------
+
+        System.out.print("Unesite a: ");
+        int a = mojs.nextInt();
+
+        if (a<0) {
+            p=a+" "+p;
+        }else {
+            p=p+" "+a;
+        }
+        System.out.println(p);
+
+//------druga_promenljiva----------------------
+
+        System.out.print("Unesite b: ");
+        int b = mojs.nextInt();
+
+        if (b<0) {
+            p=b+" "+p;
+        }else {
+            p=p+" "+b;
+        }
+        System.out.println(p);
+
+//------treca_promenljiva----------------------
+
+        System.out.print("Unesite c: ");
+        int c = mojs.nextInt();
+
+        if (c<0) {
+            p=c+" "+p;
+        }else {
+            p=p+" "+c;
+        }
+        System.out.println(p);
+
+//------cetvrta_promenljiva----------------------
+
+        System.out.print("Unesite d: ");
+        int d = mojs.nextInt();
+
+        if (d<0) {
+            p=d+" "+p;
+        }else {
+            p=p+" "+d;
+        }
+        System.out.println("String p ima vrednost = "+p);
+
+
+
+    }
+}
