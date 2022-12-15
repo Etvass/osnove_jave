0ce4068dd34246b3e7d6749bb1eb5ed7e705d89e
Luka Andrejevic <lukaandrejevic88@gmail.com>
2022-12-15 07:45:38 +0100

    ZadatakZaVezbuBr6

---
+++ src/main/java/d13_12_2022/Zadatak6.java
@@ -0,0 +1,29 @@
+package d13_12_2022;
+
+public class Zadatak6 {
+    public static void main(String[] args) {
+
+        System.out.println("        "+"*******************************************");
+        System.out.println("        "+"*                                         *");
+        System.out.println("        "+"*          Register                       *");
+        System.out.println("        "+"*              ____________________       *");
+        System.out.println("        "+"*   Email:    |                    |      *");
+        System.out.println("        "+"*              ********************       *");
+        System.out.println("        "+"*                                         *");
+        System.out.println("        "+"*              ____________________       *");
+        System.out.println("        "+"*   Password: |                    |      *");
+        System.out.println("        "+"*              ********************       *");
+        System.out.println("        "+"*                                         *");
+        System.out.println("        "+"*                                         *");
+        System.out.println("        "+"*                                         *");
+        System.out.println("        "+"*   |_| I Agree                           *");
+        System.out.println("        "+"*                                         *");
+        System.out.println("        "+"*            ___________________          *");
+        System.out.println("        "+"*           |                   |         *");
+        System.out.println("        "+"*           |    Register       |         *");
+        System.out.println("        "+"*           |___________________|         *");
+        System.out.println("        "+"*                                         *");
+        System.out.println("        "+"*******************************************");
+
+    }
+}
