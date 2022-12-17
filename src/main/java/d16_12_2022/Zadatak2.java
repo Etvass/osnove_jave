c37fb78ee06027b42d31de299533e0eb3e240791
Luka Andrejevic <lukaandrejevic88@gmail.com>
2022-12-17 10:56:47 +0100

    veomaBrzoUradjen

---
+++ src/main/java/d16_12_2022/Zadatak2.java
@@ -0,0 +1,15 @@
+package d16_12_2022;
+
+public class Zadatak2 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 25; i++) {
+            if (i%5==0) {
+                System.out.println(" _");
+            }else {
+                System.out.print(" _");
+            }
+        }
+
+    }
+}
