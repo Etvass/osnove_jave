c66360186f0e2a563366970cdb89c1746f19ba4e
Luka Andrejevic <lukaandrejevic88@gmail.com>
2022-12-22 20:49:33 +0100

    dodatakZaVezbanje

---
+++ src/main/java/d19_12_2022/Zadatak5ZaVezbanje.java
@@ -0,0 +1,45 @@
+package d19_12_2022;
+
+import java.lang.Math;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Zadatak5ZaVezbanje {
+    public static void main(String[] args) {
+
+//        5. (Za vezbanje) Napisati program za ucenje matematike.
+//        Program daje korisniku 5 matematickih zadataka.
+//        Svaki nivo predstavlja jednu iteraciju petlje i u
+//        svakom nivou korisniku se prikazuju dva random broja
+//        koja treba da sabere i poruka o tacnosti resenja.
+//        (Program generise random brojeve u opsegu od 0 do 50, ne unosi ih korisnik)
+//        • Random brojevi u Javi se generisisu kao u primeru
+          // creating an object of Random class
+//        Random random = new Random();
+          // Generates random integers 0 to 49
+//        int x = random.nextInt(50);
+//        Koristan link, koristan video
+//        Primer izvrsenja:
+//        Koliko je 1 + 4? 5
+//        Cestitam!
+//                Koliko je 3 + 34? 23
+//        Greska!
+//                Koliko je 23 + 12? 45
+//        Greska!
+//                Koliko je 23 + 12? 24
+//        Greska!
+//                Koliko je 1+ 4? 5
+//        Cestitam!
+
+                Scanner mojs = new Scanner(System.in);
+
+//        Random
+
+
+
+
+    }
+
+
+    }
+
