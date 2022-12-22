a0ad658e95e047744851916e22a58061470e0614
Luka Andrejevic <lukaandrejevic88@gmail.com>
2022-12-22 15:12:43 +0100

    nebitno

---
+++ src/main/java/d20_12_2022/DrugiEksperiment.java
@@ -0,0 +1,21 @@
+package d20_12_2022;
+
+import java.util.Scanner;
+
+public class DrugiEksperiment {
+    public static void main(String[] args) {
+
+        Scanner mojs = new Scanner(System.in);
+
+        int x;
+
+        for (x=8; x<=10;) {
+
+            System.out.println("Nesto nebitno.");
+        }
+
+        System.out.print("Unesite x: ");
+        x = mojs.nextInt();
+
+    }
+}
