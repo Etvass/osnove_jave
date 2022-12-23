92ff185d8d8080759a7263e2995bd9cbdc7a22c7
Luka Andrejevic <lukaandrejevic88@gmail.com>
2022-12-23 16:36:38 +0100

    dobroJeRadi

--- src/main/java/d21_12_2022/Zadatak4.java
+++ src/main/java/d21_12_2022/Zadatak4.java
@@ -6,6 +6,21 @@ import java.util.Scanner;
 public class Zadatak4 {
     public static void main(String[] args) {
 
+//        4. . Napisati program koji ucitava niz A duzine N i broj X.
+//        Nakon unosa treba ispisati indekse onih clanova niza koji su jednaki broju X.
+//                Primer izvrsenja:
+//        Unesite N: 5
+//        Unesite broj: 1
+//        Unesite broj: 3
+//        Unesite broj: 7
+//        Unesite broj: 3
+//        Unesite broj: 9
+//        Unesite X: 3
+//
+//        Rezultat: Elementi niza A koji su jednaki broju X imaju indekse:
+//        1, 3
+
+
         ArrayList<Integer> A = new ArrayList<Integer>();
         ArrayList<Integer> listaX = new ArrayList<Integer>();
 
