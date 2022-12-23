ef22dac5c339045e7be7284c374cf443d130694e
Luka Andrejevic <lukaandrejevic88@gmail.com>
2022-12-23 16:34:45 +0100

    probaDaLiRadi

--- src/main/java/d21_12_2022/Zadatak2.java
+++ src/main/java/d21_12_2022/Zadatak2.java
@@ -6,6 +6,17 @@ import java.util.Scanner;
 public class Zadatak2 {
     public static void main(String[] args) {
 
+//        2. .Zadatak
+//        Napisati program koji ucitava N brojeva i smesta ih u niz.
+//        Zatim, iz niza, broji koliko je parnih brojeva uneto. brojeve unosi korisnik.
+//                Unesite N: 5
+//        Unesite broj: 1
+//        Unesite broj: 3
+//        Unesite broj: 4
+//        Unesite broj: 7
+//        Unesite broj: 8
+//        U nizu ima 2 parna broja.
+
         ArrayList<Integer> brojevi = new ArrayList<Integer>();
 
         Scanner mojs = new Scanner(System.in);
