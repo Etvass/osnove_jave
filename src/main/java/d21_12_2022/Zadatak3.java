e016561b9247e40dcf562d9f7da858401554759d
Luka Andrejevic <lukaandrejevic88@gmail.com>
2022-12-23 16:35:44 +0100

    dobroJeRadi

--- src/main/java/d21_12_2022/Zadatak3.java
+++ src/main/java/d21_12_2022/Zadatak3.java
@@ -6,6 +6,19 @@ import java.util.Scanner;
 public class Zadatak3 {
     public static void main(String[] args) {
 
+//        3. .Zadatak
+//        Napisati program koji ucitava niz A duzine N, i koji
+//        nakon unosa niz A stampa sve elemente niza koji su veci od nule.
+//        Primer izvrsenja:
+//        Unesite N: 5
+//        Unesite broj: 1
+//        Unesite broj: -4
+//        Unesite broj: 3
+//        Unesite broj: -7
+//        Unesite broj: 9
+//
+//        Brojevi veci od nule u nizu A su: 1, 3, 9,
+
         ArrayList<Integer> A = new ArrayList<Integer>();
         ArrayList<Integer> veciOdNule = new ArrayList<Integer>();
 
@@ -32,7 +45,7 @@ public class Zadatak3 {
             }else {
                 System.out.print(veciOdNule.get(i)+", ");
             }
-            
+
         }
 
     }
