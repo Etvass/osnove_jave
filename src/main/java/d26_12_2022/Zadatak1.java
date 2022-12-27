7b594daad7ec562a029e4a82355dcebb13339826
Luka Andrejevic <lukaandrejevic88@gmail.com>
2022-12-27 01:11:15 +0100

    nebitno

---
+++ src/main/java/d26_12_2022/Zadatak1.java
@@ -0,0 +1,22 @@
+package d26_12_2022;
+
+public class Zadatak1 {
+    public static void main(String[] args) {
+
+        stampajVrednostZa10Vecu(9);
+        stampajVrednostZa10Vecu(4);
+        stampajVrednostZa10Vecu(3);
+        stampajVrednostZa10Vecu(1);
+        stampajVrednostZa10Vecu(9);
+        stampajVrednostZa10Vecu(7);
+        stampajVrednostZa10Vecu(56);
+
+    }
+
+    public static void stampajVrednostZa10Vecu(int x) {
+        x=x+10;
+        System.out.println("Uvecana vrednost je: "+x);
+
+    }
+
+}
