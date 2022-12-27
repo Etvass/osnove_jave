d704b395662551ff9fe347c5067308746a4158c7
Luka Andrejevic <lukaandrejevic88@gmail.com>
2022-12-27 15:54:27 +0100

    update

--- src/main/java/d26_12_2022/Zadatak1.java
+++ src/main/java/d26_12_2022/Zadatak1.java
@@ -3,6 +3,10 @@ package d26_12_2022;
 public class Zadatak1 {
     public static void main(String[] args) {
 
+//        Napisati metodu stampajVrednostZa10Vecu u kojoj se
+//        stampa prosledjena vrednost za 10 veca.
+//        Iz main-a pozvati izvrsenje metode za razlicite vrednosti.
+
         stampajVrednostZa10Vecu(9);
         stampajVrednostZa10Vecu(4);
         stampajVrednostZa10Vecu(3);
