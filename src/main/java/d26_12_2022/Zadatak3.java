699006a86c64379ee5358b275fc4c905cea0ed53
Luka Andrejevic <lukaandrejevic88@gmail.com>
2022-12-27 15:48:22 +0100

    lagan

---
+++ src/main/java/d26_12_2022/Zadatak3.java
@@ -0,0 +1,18 @@
+package d26_12_2022;
+
+public class Zadatak3 {
+
+    public static void main(String[] args) {
+
+        int c = noviBroj(7,3);
+        System.out.println(c);
+
+
+    }
+    public static int noviBroj(int x, int y) {
+        int z = x*10+y;
+        return z;
+
+    }
+
+}
