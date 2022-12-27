0d9f9cc642d1b830ef01959e94babb340623caa6
Luka Andrejevic <lukaandrejevic88@gmail.com>
2022-12-27 15:53:19 +0100

    update

--- src/main/java/d26_12_2022/Zadatak2.java
+++ src/main/java/d26_12_2022/Zadatak2.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class Zadatak2 {
     public static void main(String[] args) {
 
+//        2. Napisati metodu koja vrsi spajanje punog imena i vraca tu vrednost.
+//        Metoda od parametara prima ime i prezime (2 parametra) i
+//        vraca jedan string tako sto spoji ime i prezime. METODA NISTA NE STAMPA.
+//        Primer poziva: Ako se metoda pozvoe za ime=”Milan” i
+//        prezime=”Jovanovic”, metoda vraca “Milan Jovanovic”
+
         String konacno = imeIPrezime("Luka","Andrejevic");
         System.out.println(konacno);
 
