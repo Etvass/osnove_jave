4d25ffecc7ac2a03838fa9067539e3708e222e81
Luka Andrejevic <lukaandrejevic88@gmail.com>
2022-12-27 15:52:13 +0100

    update

--- src/main/java/d26_12_2022/Zadatak3.java
+++ src/main/java/d26_12_2022/Zadatak3.java
@@ -4,6 +4,12 @@ public class Zadatak3 {
 
     public static void main(String[] args) {
 
+//        3. Napisati funkciju koja za dva jednocifrena broja koja su
+//        ulazni parametri funkcije vraca novu vrednost koja se formira kao na primeru.
+//        METODA NISTA NE STAMPA.
+//        ako je prvi broj =2, a drugi =3 vraca novu vrednost 23.
+//        ako je prvi broj =6, a drugi =2 vraca novu vrednost 62.
+
         int c = noviBroj(7,3);
         System.out.println(c);
 
