a74f75cd3d5db96d69b97a6ce027c9e1b580e11a
Luka Andrejevic <lukaandrejevic88@gmail.com>
2022-12-28 17:31:13 +0100

    updateZaDrugi

--- src/main/java/d27_12_2022/Zadatak2/Zadatak2.java
+++ src/main/java/d27_12_2022/Zadatak2/Zadatak2.java
@@ -3,6 +3,21 @@ package d27_12_2022.Zadatak2;
 public class Zadatak2 {
     public static void main(String[] args) {
 
+//        2. Kreirati klasu SmartAirConditioning koja ima:
+//    • atribut za marku klime (npr: Samsung, Galanz, Gree, …)
+//    • atribut za izabranu temperaturu
+//    • atribut za mod (hladi/greje) - string
+//    • metodu za stampu (proizvoljno)
+//    • metodu koja vraca informaciju da li je
+//    napolju veca temperatura od one na koju je klima postavljena.
+//    Metoda kao parametar prima temperaturu koja je napolju.
+//
+//
+//        U glavnom programu kreirati minimum 2 objekta klase SmartAirConditioning i
+//        za svaki od njih pozvati metode
+
+
+
         SmartAirConditioning mojaKlima = new SmartAirConditioning();
         mojaKlima.marka = "LG";
         mojaKlima.hladGrej = "grejanje";
