7565e8e83bba5b80293cabbec30c17960e7d4ddc
Luka Andrejevic <lukaandrejevic88@gmail.com>
2022-12-28 17:29:40 +0100

    update

--- src/main/java/d27_12_2022/Zadatak1/Zadatak1.java
+++ src/main/java/d27_12_2022/Zadatak1/Zadatak1.java
@@ -3,6 +3,24 @@ package d27_12_2022.Zadatak1;
 public class Zadatak1 {
     public static void main(String[] args) {
 
+//        1. Napisati klasu Proizvod koja ima atribute
+//        ◦ naziv proizvoda (String)
+//        ◦ cenu proizvoda (double)
+//        ◦ težinu proizvoda u gramima. (double)
+//                i metode:
+//        ◦ stampaj - stampa podatke proizvoda u formatu {{naziv}}, {{cena}}, {{tezina}}
+//        ◦ konvertuj - metoda konvertuje tezinu u
+//        kilograme i tone. Metoda kao parametar prima jedinicu u
+//        koju zelimo da konvertujemo, a metoda vraca sracunatu vrednost.
+//        Jedinica moze biti “kg” ili “t”
+//        Primer: ako proizvod ima 1200 grama i pozovemo metodu sa parametrom kg, metoda vraca 1.200
+//        Primer: ako proizvod ima 12000 grama i pozovemo metodu sa parametrom t, metoda vraca 0.012
+//
+//
+//        U glavnom programu kreirati minimum 2 proizvoda i za svaki od njih pozvati metode
+
+
+
         Proizvod secer = new Proizvod();
         secer.naziv = "Kocka";
         secer.cena = 200;
