7cb21bafc4402c58aba6413aab7c97d59c3ffb3d
Luka Andrejevic <lukaandrejevic88@gmail.com>
2023-01-11 19:26:11 +0100

    update

--- src/main/java/d10_1_2023/Zadatak1.java
+++ src/main/java/d10_1_2023/Zadatak1.java
@@ -1,6 +1,27 @@
 package d10_1_2023;
 
 public class Zadatak1 {
+
+//        1. Kreirati klasu Autor koja ima
+//-ime i prezime
+//-konstuktore koje mislite da su vam potrebni
+//-gettere i settere za atribute
+//-metodu print koja stampa u formatu:
+//            (ime autora) (prezime autora)
+//
+//    Kreirati klasu Knjiga koji ima:
+//            -ISBN
+//-naziv
+//-godina izdanja
+//-autor
+//-konstuktore koje mislite da su vam potrebni
+//-gettere i settere za atribute
+//-metodu print koja stampa u formatu (izbegavati dupliranje koda):
+//            (ISBN)
+//            (naziv) - (godina izdanja)
+//    autor: (ime autora) (prezime autora)
+//
+//    U glavnom programu napraviti vise autora sa vise knjiga.
     public static void main(String[] args) {
 
         Autor prvi = new Autor("Petko","Cvetkovic");
